1be864173b2a140c7a85428f2b0fcb5d7d91420b
ztaglia <ztaglia@umich.edu>
2024-02-06 23:30:32 -0500

    Added load.py

---
+++ shazamclone-code/load.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pydub import AudioSegment
+from io import BytesIO
+import requests
+import sqlite3
+import os
+
+# TODO: Load wav file paths (not the files themselves! store those in a directory within shazamclone-code) 
+#       and their respective song titles and artist names into songs.db
+
+file_path = None
+
+df = pd.read_csv(file_path)
+
+conn = sqlite3.connect('songs.db')
+c = conn.cursor()
+
+for n in range(1):
+  songname = # TODO
+  artist = # TODO
+  mp3_url = # TODO
+
+  # TODO
+
+  # CHANGE FORMAT FROM STEREO TO MONO!
+
+  # EXPORT WAV AUDIO FILES TO A FOLDER IN SHAZAMCLONE-CODE
+
+  data = [(songname, artist, wav_audio_path)]
+
+c.executemany(
+  "INSERT INTO your_table_name (songname, artist, audio_path) VALUES (?, ?, ?)", 
+  data
+)
+
+conn.commit()
+conn.close()
